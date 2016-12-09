7825a7549f1ab1798a4bad601f2542414f9b569e
James Wang <jameswang9909@hotmail.com>
2016-12-09 09:46:59 -0500

    Work on writer.c

--- writer.c
+++ writer.c
@@ -5,38 +5,39 @@
 #include <sys/sem.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 union semun{
-  int val;
-  struct semid_ds *buff;
-  unsigned long *buffer;
-  struct seminfo *_buf;
+    int val;
+    struct semid_ds *buff;
+    unsigned long *buffer;
+    struct seminfo *_buf;
 };
 
 int main(){
-  int semkey = ftok("Makefile" , 22);
-  int shmkey = ftok("control.c", 12);
+    int semkey = ftok("Makefile" , 22);
+    int shmkey = ftok("control.c", 12);
 
-  int semid = semget(semkey, 1, 0);
-  union semun data;
-  semctl(semid, 0, -1, data);
+    int semid = semget(semkey, 1, IPC_CREAT | 0644);
+    union semun data;
+    semctl(semid, 0, -1, data);
 
-  int md = shmget(shmkey, sizeof(int), 0);
-  int p;
-  shmat(md, &p, 0);
+    int md = shmget(shmkey, sizeof(int), 0644);
+    int *len = shmat(md, 0, 0);
 
-  int fd = open("story", O_RDWR | O_APPEND);
-  char line[256];
-  read(fd, line, p);
-  printf("%s\n", line);
+    int fd = open("story", O_CREAT | O_RDWR | O_APPEND, 0644);
+    lseek(fd, -(*len), SEEK_END);
+    char *buf = (char *) malloc(*len + 1);
 
-  printf("What is the next line? ");
-  char *input;
-  fgets(input, 256, stdin);
+    read(fd, buf, sizeof(buf));
+    printf("%s", buf);
 
-  write(fd, input, sizeof(input));
-  close(fd);
-  p = sizeof(input);
-  shmdt(&p);
+    printf("What is the next line? ");
+    char input[256];
+    fgets(input, 256, stdin);
 
+    write(fd, input, strlen(input));
+    close(fd);
+    *len = strlen(input);
+    shmdt(len);
 }
